b2a5d4360fcf5d492629885f3c569e6f9434116a
vagner santos <vagner131417@gmail.com>
2022-11-09 14:23:32 -0300

    home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import type { AppProps } from 'next/app'
+import { Header } from '../components/Header';
+
 import '../styles/global.scss';
 
-import type { AppProps } from 'next/app'
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default class MyDocument extends Document {
               <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Nunito:wght@400;700&family=Roboto:wght@900&display=swap" rel="stylesheet" />
+
+              <link rel="shortcut icon" href="/favicon.png" type="image/png" />
             </>
           }
         </Head>
