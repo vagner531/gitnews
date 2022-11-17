4bd95a757dd6a63fd0072f7bca1928fb642048a0
vagner santos <vagner131417@gmail.com>
2022-11-16 23:43:11 -0300

    update

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -8,7 +8,8 @@ async function buffer(readable: Readable) {
 
   for await (const chunk of readable) {
     chunks.push(
-      typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
   }
 
   return Buffer.concat(chunks);
@@ -17,32 +18,30 @@ async function buffer(readable: Readable) {
 export const confing = {
   api: {
     bodyParser: false
-  }
-}
+  },
+};
 
 const relevantEvents = new Set(['checkout.session.completed'])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const buf = await buffer(req)
-    const secret  = req.headers['stripe-signature']
+    const secret = req.headers['stripe-signature']
 
-      let event: Stripe.Event;
+    let event: Stripe.Event;
 
-      try {
-        event = stripe.webhooks.constructEvent(
-          buf, 
-          secret, 
-          process.env.STRIPE_WEBHOOKS_SECRET);
-      } catch (err) {
-        return res.status(400).send(`Webhook error: ${err.message}`);
-      }
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
 
-      const { type }  = event;
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
 
-      if (!relevantEvents.has(type)) {
-        console.log('Evento Recebido', event)
-      }
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      console.log('Evento Recebido', event)
+    }
 
     res.json({ received: true })
   } else {
