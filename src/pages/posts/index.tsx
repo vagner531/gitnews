1f0065b9795ab89dd49ab8e237aed7c75ae552b4
vagner santos <vagner131417@gmail.com>
2022-11-25 12:27:32 -0300

    create posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return(
+    <>
+    <Head>
+      <title>Posts | Git.News</title>
+    </Head>
+
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <a href="#">
+          <time>12 de março de 2022</time>
+          <strong>Creating a Monorego with Lern & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+        <a href="#">
+          <time>12 de março de 2022</time>
+          <strong>Creating a Monorego with Lern & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+        <a href="#">
+          <time>12 de março de 2022</time>
+          <strong>Creating a Monorego with Lern & Yarn Workspaces</strong>
+          <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+        </a>
+      </div>
+    </main>
+    </>
+  );
+}
